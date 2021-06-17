b8e4bf00aaea6778f43e32f462b4c7647c76b3bb
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-18 00:22:33 +0530

    modified : Unique set and distribution

--- src/Deck/DeckOfCards.java
+++ src/Deck/DeckOfCards.java
@@ -1,46 +1,67 @@
 package Deck;
 
-public class DeckOfCards {
-	
-	   
-	      private static String[] SUITS= new  String[]  {
-	            "Clubs", "Diamonds", "Hearts", "Spades"
-	        };
-	      private static String[] RANKS = new String[] {
-	            "2", "3", "4", "5", "6", "7", "8", "9", "10",
-	            "Jack", "Queen", "King", "Ace"
-	        };
-//	      private static String[] = new String[52];
-	      public static void main(String[] args) {
-	        // initialize deck
-//	      private void SuitDeck() {
-	    	  int n = SUITS.length * RANKS.length;	 
-		        String[] deck = new String[n];
-	        for (int i = 0; i < RANKS.length; i++) {
-	            for (int j = 0; j < SUITS.length; j++) {
-	            	
-	               deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
-	            }
-	        }
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-	       
-	        // shuffle
-	        for (int i = 0; i < n; i++) {
-	            int r = i + (int) (Math.random() * (n-i));
-	            String temp = deck[r];
-	            deck[r] = deck[i];
-	            deck[i] = temp;
-	        }
-	      
-	      
-	        	
-	        // print shuffled deck
-	        for (int i = 0; i < n; i++) {
-	            System.out.println(deck[i]);
-	        }
-	    }
+public class DeckOfCards {
+
+	private String[] suits = new String[] { "Clubs", "Diamonds", "Hearts", "Spades" };
+	private String[] rank = new String[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",	"Ace" };
+	String[] card = new String[52];
+	int start = 0, end = 12, j1 = 0;
+
+	/**
+	 * for combination of Suits and Ranks.
+	 *  Created HashSet to check if all cards are unique.
+	 * 
+	 * @return string array deck of cards if all cards are unique
+	 */
+	private String[] Deck() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = start; j <= end; j++) {
+				card[j] = suits[i] + " " + rank[j1];
+
+			}
+			j1 = 0;
+			start = end + 1;
+			end += 13;
+			Set<String> checkSet = new HashSet<String>(Arrays.asList(card));
+
+			if (checkSet.size() == card.length) {
+				return card;
+			} else {
+				return null;
+			}
+		}
+
+		/**
+		 * code for shuffle all the cards
+		 * 
+		 */
+		Utility.shuffleCards(card);
+
+		/**
+		 * printing cards for 4 player distributed is done to receive
+		 *  all 9 cards for each
+		 */
+
+		int j2 = 0;
+		for (int i = 0; i < 4; i++) {
+			System.out.print(" Player " + i + ": \n");
+			for (int j = 0; j < 9; j++) {
+
+				System.out.print(card[j2] + "== \n");
+				j2++;
+			}
+			System.out.println(card);
+		}
+		return card; 
 
 	}
-	
-	
 
+	public static void main(String[] args) {
+		DeckOfCards card = new DeckOfCards();
+		card.Deck();
+	}
+}
---
+++ src/Deck/Utility.java
@@ -0,0 +1,31 @@
+package Deck;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Utility {
+
+	static Scanner scanner = new Scanner(System.in);	
+	/**   
+	 * shuffles the card of deck
+	 * @param arr array of cards
+	 * @return returns shuffle array
+	 */
+	public static String[] shuffleCards(String[] arr) {
+		Random random = new Random();
+		int a, b;
+		String temp;
+		for (int i = 0; i < 52; i++) {
+			a = random.nextInt(51);
+			b = random.nextInt(51);
+			if (a != b) {
+				temp = arr[a];
+				arr[a] = arr[b];
+				arr[b] = temp;
+			}
+		}
+		return arr;
+
+	}
+
+}
